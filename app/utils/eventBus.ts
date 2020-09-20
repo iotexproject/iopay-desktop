998e8638ed9b083a508210486856eb7065ed89a4
uuz <ncysatnaf@gmail.com>
2020-09-20 15:19:15 +0800

    fix build error

--- app/utils/eventBus.ts
+++
@@ -1,8 +0,0 @@
-import { EventEmitter } from 'events';
-import TypedEmitter from 'typed-emitter';
-
-interface MessageEvents {
-  'client.inited': () => void;
-}
-
-export const eventBus = new EventEmitter() as TypedEmitter<MessageEvents>;

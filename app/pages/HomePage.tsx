b075a52d52d5845926d82ed786dde2895ac566be
ZQ-jhon <350037310@qq.com>
2020-09-21 20:43:55 +0800

    chore: adding packages and remove unused files

--- app/pages/HomePage.tsx
+++ app/pages/HomePage.tsx
@@ -1,15 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { routes } from '../constants/routes';
-import { cssUtils } from '../features/stitches/css';
 
 export default function HomePage() {
-  return (
-    <div className={cssUtils.page}>
-      <div>
-        <h2>Home</h2>
-        <Link to={routes.COUNTER.path}>to Counter</Link>
-      </div>
-    </div>
-  );
+  return <div>123</div>;
 }
